348c783f0462d302f01cae7e0e25ca5c0892731e
HenintsoaRatsim <ratsimhenintsoa@gmail.com>
2024-04-06 11:42:35 +0300

    Déplacement du package JSF vers le répertoire global du projet

--- src/main/java/com/ratsimbazafy/tp2prgratsimbazafy/jsf/Bean.java
+++ src/main/java/com/ratsimbazafy/tp2prgratsimbazafy/jsf/Bean.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
-package jsf;
+package com.ratsimbazafy.tp2prgratsimbazafy.jsf;
 
 import java.util.ArrayList;
 import java.util.List;
